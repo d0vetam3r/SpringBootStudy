a994e1f911ef5f6173a598588d95e5847e21b1eb
d0vetam3r <d0vetam3r@gmail.com>
2022-08-18 11:58:10 +0900

    first commit

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,30 @@
+package hello.hellospring;
+
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class SpringConfig {
+   private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+   // @Bean
+    //public MemberRepository memberRepository(){
+       // return new JdbcTempleteMemberRepository(dataSource);
+       // return new JpaMemberRepository(em);
+   // }
+}
